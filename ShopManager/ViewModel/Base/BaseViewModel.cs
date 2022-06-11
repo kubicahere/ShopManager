440362e99303a6a566f4f232504784e75d4b4d1f
kubica.here <kubica.here@gmail.com>
2022-06-11 18:36:21 +0200

    Dodaj pliki projektów.

---
+++ ShopManager/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManager.ViewModel.Base
+{
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(params string[] namesOfProperties)
+        {
+            if (PropertyChanged != null)
+            {
+                foreach (var prop in namesOfProperties)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(prop));
+                }
+            }
+        }
+    }
+}
