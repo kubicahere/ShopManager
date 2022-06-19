95c0214e8124b941788adf23356bdf9d6b3d63da
Venrofi <radoslaw.terelak@gmail.com>
2022-06-19 19:45:41 +0200

    RootMainWindow updates

--- ShopManager/ViewModel/RootLoginViewModel.cs
+++ ShopManager/ViewModel/RootLoginViewModel.cs
@@ -19,6 +19,11 @@ namespace ShopManager.ViewModel
         private ObservableCollection<string> _listOfProducts;
         private ObservableCollection<Purchase> _listOfPurchases;
         private string _selectedProduct = null;
+
+        private string _isVisibleRootWindow = "Visible";
+        private string _isVisibleProducts = "Hidden";
+        private string _isVisibleWorkersList = "Hidden";
+        private string _isVisiblePurchaseList = "Hidden";
         #endregion
 
         #region Getters & setters
@@ -28,6 +33,10 @@ namespace ShopManager.ViewModel
         public ObservableCollection<string> listOfProducts { get { return _listOfProducts; } set { _listOfProducts = value; OnPropertyChanged(nameof(listOfProducts)); } }
         public ObservableCollection<Purchase> listOfPurchases { get { return _listOfPurchases; } set { _listOfPurchases = value; OnPropertyChanged(nameof(listOfPurchases)); } }
         public string selectedProduct { get { return _selectedProduct; } set { _selectedProduct = value; OnPropertyChanged(nameof(selectedProduct)); } }
+        public string isVisibleRootWindow { get { return _isVisibleRootWindow; } set { _isVisibleRootWindow = value; OnPropertyChanged(nameof(isVisibleRootWindow)); } }
+        public string isVisibleProducts { get { return _isVisibleProducts; } set { _isVisibleProducts = value; OnPropertyChanged(nameof(isVisibleProducts)); } }
+        public string isVisibleWorkersList { get { return _isVisibleWorkersList; } set { _isVisibleWorkersList = value; OnPropertyChanged(nameof(isVisibleWorkersList)); } }
+        public string isVisiblePurchaseList { get { return _isVisiblePurchaseList; } set { _isVisiblePurchaseList = value; OnPropertyChanged(nameof(isVisiblePurchaseList)); } }
         #endregion
 
         #region ViewModel instances
@@ -67,7 +76,28 @@ namespace ShopManager.ViewModel
             startWindow.isVisible = "Visible";
             isVisible = "Hidden";
         }
-        //TODO WINDOW METHODS
         #endregion
+        #region Window methods
+        public void LogOut(object sender)
+        {
+            rootMainWindow.Close();
+        }
+        public void AddProducts(object sender)
+        {
+            isVisibleRootWindow = "Hidden";
+            isVisibleProducts = "Visible";
+        }
+        public void WorkersList(object sender)
+        {
+            isVisibleRootWindow = "Hidden";
+            isVisibleWorkersList = "Visible";
+        }
+        public void PurchaseList(object sender)
+        {
+            isVisibleRootWindow = "Hidden";
+            isVisiblePurchaseList = "Visible";
+        }
+        #endregion
+
     }
 }
